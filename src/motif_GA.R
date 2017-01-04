0af3253c7d5dbd6f2ea257dd61e2ece781a312b5
bgudenas <bgudena@g.clemson.edu>
2017-01-04 17:45:10 -0500

    Set up GA

--- src/motif_GA.R
+++ src/motif_GA.R
@@ -1,10 +1,35 @@
 library(GA)
 
+source("./src/Sequence_Functions.R")
+test_seqs = Motif_Implanter(seqs = Rando_DNA_Strings(seq_num = 20, width = 30), motif ="GGGGGGGG" )
+background = Motif_Implanter(seqs = Rando_DNA_Strings(seq_num = 20, width = 30), motif ="" )
 
-GA=ga("real-valued", min = rep(0, length(test_seqs)), max = unlist(lapply(test_seqs, function(x) nchar(x))),
-      fitness=itness, crossover = FB_crossover, 
-      mutation = mutation, popSize=1000, maxiter=100,
-      pmutation= 0.3, pcrossover=0.7,run=20)
+Seqs = c(test_seqs, background)
+names(Seqs)[1:20] = "Positive"
+names(Seqs)[21:40] = "Negative"
+
+pos = 20
+motif_width = 8
+
+pos.freq = letterFrequency(DNAStringSet(Seqs), letters = DNA_BASES)[1:pos, ]
+pos.params = colSums(pos.freq)/sum(pos.freq)
+
+neg.freq = letterFrequency(DNAStringSet(Seqs), letters = DNA_BASES)[(pos+1):length(Seqs), ]
+neg.params = colSums(neg.freq)/sum(neg.freq)
+
+GA=ga("real-valued", min = rep(1, length(Seqs)), max = unlist(lapply(Seqs, function(x) nchar(x)-motif_width)),
+      fitness = PWM_dist, population = seeder,
+      # fitness=itness, crossover = FB_crossover,population = seeder,
+      popSize=1000, maxiter=1,
+      pmutation= 0.3, pcrossover=0.7,run=1)
+
+
+
+
+
+
+
+
 
 
 f <- function(x) abs(x)+cos(x)
