d3e8789ad20d867d6d65506874c326f2b893270e
bgudenas <bgudena@g.clemson.edu>
2017-01-26 12:20:51 -0500

    deprecated

--- src/motif_GA.R
+++ src/motif_GA.R
@@ -1,35 +1,34 @@
 library(GA)
+library(seqLogo)
 
 source("./src/Sequence_Functions.R")
-test_seqs = Motif_Implanter(seqs = Rando_DNA_Strings(seq_num = 20, width = 30), motif ="GGGGGGGG" )
-background = Motif_Implanter(seqs = Rando_DNA_Strings(seq_num = 20, width = 30), motif ="" )
+Seqs = Motif_Implanter(seqs = Rando_DNA_Strings(seq_num = 400, width = 300), motif ="GGGGGGGG" )
+background = Motif_Implanter(seqs = Rando_DNA_Strings(seq_num = 200, width = 300), motif ="" )
+# 
+# Seqs = c(test_seqs, background)
+# names(Seqs)[1:200] = "Positive"
+# names(Seqs)[201:400] = "Negative"
 
-Seqs = c(test_seqs, background)
-names(Seqs)[1:20] = "Positive"
-names(Seqs)[21:40] = "Negative"
-
-pos = 20
+positives = 400
 motif_width = 8
 
-pos.freq = letterFrequency(DNAStringSet(Seqs), letters = DNA_BASES)[1:pos, ]
-pos.params = colSums(pos.freq)/sum(pos.freq)
+priors = letterFrequency(DNAStringSet(Seqs), letters = DNA_BASES)
+prior.params = colSums(priors)/sum(priors)
 
-neg.freq = letterFrequency(DNAStringSet(Seqs), letters = DNA_BASES)[(pos+1):length(Seqs), ]
-neg.params = colSums(neg.freq)/sum(neg.freq)
 
-GA=ga("real-valued", min = rep(1, length(Seqs)), max = unlist(lapply(Seqs, function(x) nchar(x)-motif_width)),
+GA=ga("binary", nBits = length(Seqs),
       fitness = PWM_dist, population = seeder,
-      # fitness=itness, crossover = FB_crossover,population = seeder,
-      popSize=1000, maxiter=1,
-      pmutation= 0.3, pcrossover=0.7,run=1)
+      crossover = pt_crossover, mutation = mutation,
+      popSize=400, maxiter=200,
+      pmutation= 0.5, pcrossover=0.6, run = 25, monitor = plot)
 
 
+motif = DNAStringSet(Seqs, start = GA@solution, end = GA@solution + (motif_width-1))
+cons = consensusMatrix(motif, as.prob = TRUE)[1:4, ]
 
 
-
-
-
-
+pwm = PWM(motif)
+seqLogo(cons)
 
 
 f <- function(x) abs(x)+cos(x)
